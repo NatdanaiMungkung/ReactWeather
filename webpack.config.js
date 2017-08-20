df01df4937e247c6acf98cf1cbb7ab2ef78b0fa6
Natdanai Mungkung <natdanai.mungkung@agoda.com>
2017-08-20 10:18:14 +0700

    Add jquery and foundation

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './public/react.jsx',
+    entry: [
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './public/react.jsx'
+    ],
+    externals:{
+        jquery: 'jQuery'
+    },
+    plugins:[
+        new webpack.ProvidePlugin({
+            '$':'jquery',
+            'jquery':'jquery'
+        })
+    ],
     output: {
         path: __dirname,
         filename: './public/bundle.js'
